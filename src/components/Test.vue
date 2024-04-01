2ee4d60f9be48d5886f6c583ea410f185fb5d153
benshrimpton <benshrimpton@users.noreply.github.com>
2024-04-01 07:43:03 -0700

    Initial commit

---
+++ src/components/Test.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+const data = ref(null);
+function foo() {
+  alert('Foo');
+}
+onMounted(() => {
+  fetch('https://jsonplaceholder.typicode.com/todos')
+    .then((response) => response.json())
+    .then((json) => (data.value = json));
+});
+</script>
+
+<template>
+  <div>
+    <div v-if="data">
+      <div v-for="i in data" :key="i.id" style="border : solid 1px:">
+        {{ i.title }}
+      </div>
+    </div>
+    <button @click="foo">buttton</button>
+  </div>
+</template>
